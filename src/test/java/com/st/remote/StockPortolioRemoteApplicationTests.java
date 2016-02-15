36cb5e92094df7b76f597bba97aafef2962d1280
sergiotorres81 <sergiotorres81@gmail.com>
2016-02-15 18:01:19 +0200

    added test. remove unused class

--- src/test/java/com/st/remote/StockPortolioRemoteApplicationTests.java
+++ src/test/java/com/st/remote/StockPortolioRemoteApplicationTests.java
@@ -2,9 +2,9 @@ package com.st.remote;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = StockPortolioRemoteApplication.class)
@@ -13,6 +13,7 @@ public class StockPortolioRemoteApplicationTests {
 
 	@Test
 	public void contextLoads() {
+		System.out.println("something");
 	}
 
 }
---
+++ src/test/java/com/st/remote/service/impl/StockServiceImplTest.java
@@ -0,0 +1,55 @@
+package com.st.remote.service.impl;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+import com.st.remote.StockPortolioRemoteApplication;
+import com.st.remote.domain.markit.Stock;
+import com.st.remote.service.StockService;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = StockPortolioRemoteApplication.class)
+@WebAppConfiguration
+public class StockServiceImplTest {
+
+	@Autowired
+	private StockService stockService;
+
+	@Test
+	public void testFindStockByTicker() {
+		System.out.println("testFindStockByTicker");
+		Stock stock = createStockForMarkit();
+		Stock remote = stockService.findStockByTicker("MCD");
+		assertEquals(stock.getSymbol(), remote.getSymbol());
+		assertEquals(stock.getStatus(), remote.getStatus());
+		assertEquals(stock.getName(), remote.getName());
+	}
+
+	private Stock createStockForMarkit() {
+		// this should come in the json response associated to MCD symbol
+		// "Status":"SUCCESS","Name":"McDonald's Corp","Symbol":"MCD"
+		Stock stock = new Stock();
+		stock.setSymbol("MCD");
+		stock.setStatus("Success");
+		stock.setName("McDonald's Corp");
+
+		return stock;
+	}
+
+	@Test
+	public void testFindStockByTickerAndMarket() {
+		Stock stock = createStockForYahoo();
+	}
+
+	private Stock createStockForYahoo() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
