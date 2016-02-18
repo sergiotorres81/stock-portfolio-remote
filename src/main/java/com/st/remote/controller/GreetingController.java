597f270103e5eb0c8ba4584a198b0da848ac0375
sergiotorres81 <sergiotorres81@gmail.com>
2016-02-18 15:16:22 +0200

    welcome message and rest API controller

--- src/main/java/com/st/remote/controller/GreetingController.java
+++ src/main/java/com/st/remote/controller/GreetingController.java
@@ -12,4 +12,19 @@ public class GreetingController {
 		return "Greetings " + name;
     }
 	
+	@RequestMapping(value = "/")
+	public String welcome() {
+		return "Welcome. Yo can find the api in /api ";
+	}
+
+	@RequestMapping(value = "/api")
+	public String api() {
+		return "API under construction"
+				+ "<br>"
+				+ "GET /ticker/{ticker} retrieves a ticker"
+				+ "<br>"
+				+ "GET /market/{market}/ticker/{ticker} retrieves a ticker ";
+	}
+
+
 }
